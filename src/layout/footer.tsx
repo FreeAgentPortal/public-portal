f01a0e0fd0d1218ec78476da22ddd5b52b54958e
Ethan Cannelongo <ethancannelongo@gmail.com>
2025-07-28 18:17:46 -0400

    Fix Logo

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -7,7 +7,7 @@ const Footer = (props: Props) => {
   return (
     <div className='mt-auto'>
       <footer className='text-center p-4 flex sm:flex-row items-center sm:justify-between border-t-[0.5px] border-gray-300 flex-col justify-center mt-5'>
-        <Image src='/Logo.png' alt='Logo' width={150} height={150} />
+        <Image src='/logo.png' alt='Logo' width={150} height={150} />
         <p className='text-sm text-gray-500'>
           &copy; {new Date().getFullYear()} Free Agent Portal. All rights
           reserved.
