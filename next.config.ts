495b0bec34a063f96ae231bbc87ae287c3308f06
Ethan Cannelongo <ethancannelongo@gmail.com>
2025-07-29 09:05:55 -0400

    Test images

--- next.config.ts
+++ next.config.ts
@@ -5,22 +5,11 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "cdn.pixabay.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.pexels.com",
-      },
-      {
-        protocol: "https",
-        hostname: "a.espncdn.com",
-      },
-      {
-        protocol: "https",
-        hostname: "nfldraftdiamonds.com",
+        hostname: "**",
+        port: "",
+        pathname: "**",
       },
     ],
-    unoptimized: true,
   },
 };
 
